c8ad7f35468dcd3c3a1a5af5e3e79bc780f23922
Djalmir <djalmir_miodutzki@hotmail.com>
2020-03-12 00:07:21 -0300

    sw alterado

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@ self.addEventListener('activate', function activator(event){
 self.addEventListener('fetch', function(event){
     event.respondWith(
         caches.match(event.request).then(function(cachedResponse){
-            return cachedResponse || fetch(event.request)
+            return fetch(event.request) || cachedResponse
         })
     )
 })
