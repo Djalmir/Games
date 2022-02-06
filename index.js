bec11704e8ea3a596996b706f73771ad52505005
Djalmir <djalmir_miodutzki@hotmail.com>
2022-02-06 04:03:30 -0300

    particles header

--- index.js
+++ index.js
@@ -6,19 +6,19 @@
 // document.onmousemove = move
 // document.ontouchmove = move
 
-let canvas = document.querySelector('#canvas')
-let c = canvas.getContext('2d')
+let bgCanvas = document.querySelector('#bgCanvas')
+let c = bgCanvas.getContext('2d')
 
 let cWidth = document.body.offsetWidth
 let cHeight = document.body.offsetHeight
-function setCanvasSizes() {
-	canvas.width = document.body.offsetWidth
-	canvas.height = document.body.offsetHeight
+function setBgCanvasSizes() {
+	bgCanvas.width = document.body.offsetWidth
+	bgCanvas.height = document.body.offsetHeight
 	cWidth = document.body.offsetWidth
 	cHeight = document.body.offsetHeight
 }
-setCanvasSizes()
-window.addEventListener('resize', setCanvasSizes)
+setBgCanvasSizes()
+window.addEventListener('resize', setBgCanvasSizes)
 
 let mousePos = {x: 0, y: 0}
 
@@ -47,7 +47,7 @@ let arcRadiusAction = 'grow'
 let breathCounter = 0
 function rgb() {
 
-	let grd = c.createRadialGradient(mousePos.x, mousePos.y, 0, mousePos.x, mousePos.y, arcRadius-50)
+	let grd = c.createRadialGradient(mousePos.x, mousePos.y, 0, mousePos.x, mousePos.y, arcRadius - 50)
 	grd.addColorStop(0, `#${ r }${ g }${ b }`)
 	grd.addColorStop(1, `#${ r }${ g }${ b }00`)
 
@@ -71,7 +71,7 @@ function rgb() {
 					arcRadiusAction = 'grow'
 				break
 		}
-		breathCounter=0
+		breathCounter = 0
 	}
 
 	if (counter >= 9) {
@@ -166,7 +166,192 @@ function showHideInterface() {
 		// document.querySelector('footer').removeChild(hideInterfaceBt)
 		eyeImg.style.opacity = '1'
 	}
+	setBgCanvasSizes()
 }
+
+
+
+const headerCanvas = document.querySelector('#headerCanvas')
+let headerC = headerCanvas.getContext('2d')
+
+headerCanvas.width = 2500
+headerCanvas.height = 400
+
+const showText = "Razion Games"
+
+let particleArray = []
+
+const mouse = {
+	x: undefined,
+	y: undefined,
+	radius: headerCanvas.width / 10
+}
+
+function setAdjusts() {
+	screenAdjustX = headerCanvas.width / headerCanvas.offsetWidth
+	screenAdjustY = headerCanvas.height / headerCanvas.offsetHeight
+	mouse.radius = headerCanvas.width / 10
+}
+setAdjusts()
+
+window.addEventListener('resize', setAdjusts)
+
+function mousemove(e) {
+	let bounds = headerCanvas.getBoundingClientRect()
+	if (e.touches) {
+		mouse.x = (e.touches[e.touches.length - 1].clientX - bounds.x) * screenAdjustX
+		mouse.y = (e.touches[e.touches.length - 1].clientY - bounds.y) * screenAdjustY
+	}
+	else {
+		mouse.x = (e.x - bounds.x) * screenAdjustX
+		mouse.y = (e.y - bounds.y) * screenAdjustY
+	}
+	mouse.radius = headerCanvas.width / 10
+}
+
+window.addEventListener('mousemove', mousemove)
+window.addEventListener('touchmove', mousemove)
+
+window.addEventListener('mouseout', () => {
+	mouse.x = undefined
+	mouse.y = undefined
+})
+window.addEventListener('touchend', () => {
+	mouse.x = undefined
+	mouse.y = undefined
+})
+
+var textCoordinates
+
+const chars = ['ﾊ', 'ﾐ', 'ﾋ', 'ｰ', 'ｳ', 'ｼ', 'ﾅ', 'ﾓ', 'ﾆ', 'ｻ', 'ﾜ', 'ﾂ', 'ｵ', 'ﾘ', 'ｱ', 'ﾎ', 'ﾃ', 'ﾏ', 'ｹ', 'ﾒ', 'ｴ', 'ｶ', 'ｷ', 'ﾑ', 'ﾕ', 'ﾗ', 'ｾ', 'ﾈ', 'ｽ', 'ﾀ', 'ﾇ', 'ﾍ']
+const colors = ['#ccc', '#ddd', '#eee', '#fff']
+
+class Particle {
+	constructor(x, y) {
+		this.x = x * 1.6
+		this.y = y * 2.5
+		this.size = 1 * headerCanvas.width / 100
+		if (this.size < 1)
+			this.size = 1
+		this.baseX = this.x
+		this.baseY = this.y
+		this.density = (Math.random() * 100) + 25
+		this.char = chars[Math.floor(Math.random() * chars.length)]
+		this.initialColor = '#fff'
+		this.color = this.initialColor
+		this.colorOnMove = colors[Math.floor(Math.random() * colors.length)]
+		setInterval(() => {
+			this.char = chars[Math.floor(Math.random() * chars.length)]
+		}, 100 + Math.floor(Math.random() * 2500))
+	}
+
+	draw() {
+		headerC.fillStyle = this.color
+		headerC.font = `bold ${ this.size }px Play`
+		headerC.fillText(this.char, this.x, this.y)
+
+		// headerC.beginPath()
+		// headerC.arc(this.x, this.y, this.size, 0, Math.PI * 2, false)
+		// headerC.closePath()
+		// headerC.fill()
+
+		// headerC.fillRect(this.x, this.y, this.size, this.size)
+	}
+
+	update() {
+		let dx = mouse.x - this.x
+		let dy = mouse.y - this.y
+		let distance = Math.sqrt(dx * dx + dy * dy)
+
+		let forceDirectionX = dx / distance
+		let forceDirectionY = dy / distance
+
+		let maxDistance = mouse.radius
+		let force = (maxDistance - distance) / maxDistance
+		let directionX = forceDirectionX * force * this.density
+		let directionY = forceDirectionY * force * this.density
+
+		if (distance < mouse.radius) {
+			this.x -= directionX
+			this.y -= directionY
+			this.color = this.colorOnMove
+			if (this.size < 2 * headerCanvas.width / 100)
+				this.size++
+		}
+		else {
+			this.color = this.initialColor
+			if (this.size > 1 * headerCanvas.width / 100)
+				this.size--
+			if (this.size < 1)
+				this.size = 1
+			if (this.x !== this.baseX) {
+				let dx = this.x - this.baseX
+				this.x -= dx / 10
+			}
+			if (this.y !== this.baseY) {
+				let dy = this.y - this.baseY
+				this.y -= dy / 10
+			}
+		}
+	}
+}
+
+function init() {
+	headerC.fillStyle = '#fff'
+	headerC.font = 'bold 16px sans-serif'
+	headerC.textAlign = 'center'
+	headerC.fillText(showText, headerCanvas.width / 30, headerCanvas.height / 25)
+	// let img = new Image()
+	// img.src = 'dj.png'
+	// img.addEventListener('load', () => {
+	// 	headerC.drawImage(img, 0, 0, 100, 100)
+
+	textCoordinates = headerC.getImageData(0, 0, headerCanvas.width, headerCanvas.height)
+
+	particleArray = []
+	// for (let i = 0; i < 1000; i++) {
+	// 	let x = Math.random() * innerWidth
+	// 	let y = Math.random() * innerHeight
+	// 	particleArray.push(new Particle(x, y))
+	// }
+	for (let y = 0, y2 = textCoordinates.height; y < y2; y++) {
+		for (let x = 0, x2 = textCoordinates.width; x < x2; x++) {
+			if (textCoordinates.data[(y * 4 * textCoordinates.width) + (x * 4) + 3] > 130) {
+				let positionX = x
+				let positionY = y
+				particleArray.push(new Particle(positionX * 10 - headerCanvas.width / 50, positionY * 10 - headerCanvas.height / 50))
+			}
+		}
+	}
+}
+
+init()
+
+function animate() {
+	headerC.clearRect(0, 0, headerCanvas.width, headerCanvas.height)
+	let opacityValue = 1
+	for (let a = 0; a < particleArray.length; a++) {
+		particleArray[a].draw()
+		particleArray[a].update()
+		// for (let b = a; b < particleArray.length; b++) {
+		// 	let dx = particleArray[a].x - particleArray[b].x
+		// 	let dy = particleArray[a].y - particleArray[b].y
+		// 	let distance = Math.sqrt(dx * dx + dy * dy)
+		// 	opacityValue = 1 - (distance/50)
+		// 	headerC.strokeStyle = `rgba(255,0,0,${opacityValue})`
+		// 	if (distance < 50) {
+		// 		headerC.lineWidth = 2
+		// 		headerC.beginPath()
+		// 		headerC.moveTo(particleArray[a].x, particleArray[a].y)
+		// 		headerC.lineTo(particleArray[b].x, particleArray[b].y)
+		// 		headerC.stroke()
+		// 	}
+		// }
+	}
+	requestAnimationFrame(animate)
+}
+animate()
+// })
 
 
 
